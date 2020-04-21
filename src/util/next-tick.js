dc8bc0fbd18987ad2d25dbabf9182ec9f2d40e4b
xiaoyufei <915724526@qq.com>
2020-04-21 15:37:31 +0800

    feat(master): add nextTick Promise part

--- src/util/next-tick.js
+++ src/util/next-tick.js
@@ -18,6 +18,26 @@ if (typeof Promise !== 'undefined') {
     timerFunc = () => {
         Promise.resolve().then(flushCallbacks)
     }
+} else if (typeof MutationObserver !== 'undefined') {
+    let counter = 1
+    const observer = new MutationObserver(flushCallbacks)
+    const textNode = document.createTextNode(String(counter))
+    observer.observe(textNode, {
+        characterData: true
+    })
+    timerFunc = () => {
+        // tips: 只需要监听变动即可，累加有风险
+        counter = (counter + 1) % 2
+        textNode.data = String(counter)
+    }
+} else if (typeof setImmediate !== 'undefined') {
+    timerFunc = () => {
+        setImmediate(flushCallbacks)
+    }
+} else {
+    timerFunc = () => {
+        setTimeout(flushCallbacks, 0)
+    }
 }
 
 /**
