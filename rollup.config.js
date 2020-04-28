e26ae921544a7981520f1e5adc28fd62f6d3250a
xiaoyufei <915724526@qq.com>
2020-04-28 18:37:39 +0800

    feat(master): build umd

--- rollup.config.js
+++ rollup.config.js
@@ -5,10 +5,11 @@ const path = require('path')
 import babel from '@rollup/plugin-babel'
 
 export default {
-    input: path.join(__dirname, './src/core/index.js'),
+    input: path.join(__dirname, './src/observer/index.js'),
     output: {
+        name: 'vue',
         file: 'build/index.js',
-        format: 'cjs'
+        format: 'umd'
     },
     plugins: [
         babel({
